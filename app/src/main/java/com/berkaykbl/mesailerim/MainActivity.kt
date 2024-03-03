c5c6f28e9bcead8a08106c9b9aa3ddb1885541ca
BerkayKBL <49043450+BerkayKBL@users.noreply.github.com>
2024-03-03 15:21:55 +0300

    Test

--- app/src/main/java/com/berkaykbl/mesailerim/MainActivity.kt
+++ app/src/main/java/com/berkaykbl/mesailerim/MainActivity.kt
@@ -2,21 +2,40 @@ package com.berkaykbl.mesailerim
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.GridView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.time.LocalDate
+import com.berkaykbl.mesailerim.Adapters.DateAdapter
+import com.berkaykbl.mesailerim.Adapters.DateItem
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var gridView: GridView? = null
+    private var arrayList: ArrayList<DateItem>? = null
+    private var dateAdapter: DateAdapter? = null
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        gridView = findViewById(R.id.calendar_grid)
+        arrayList = ArrayList()
+        arrayList = setDataList()
+        dateAdapter = DateAdapter(applicationContext, arrayList!!)
+        gridView?.adapter = dateAdapter
 
-        val day = LocalDate.now().dayOfMonth
-        val month = LocalDate.now().month
-        val year = LocalDate.now().year
-        val day1 = LocalDate.of(year, month, 1)
-        println(month)
-        println(day)
-        println(day1)
+    }
+
+    private fun setDataList(): ArrayList<DateItem> {
+        var arrayList: ArrayList<DateItem> = ArrayList()
+
+        val dateFormat: DateFormat = SimpleDateFormat("MM")
+        val date = Date()
+        Log.d("Month", dateFormat.format(date))
+        return arrayList
     }
 }
